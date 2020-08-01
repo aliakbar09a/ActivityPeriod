de2f24904c29fa9e59379cca361ff41b52b0338a
Ali Akbar <aliakbar09a@gmail.com>
2020-08-01 23:32:24 +0530

    django_heroku removed

--- jsonResponse/settings.py
+++ jsonResponse/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,4 +124,4 @@ STATIC_URL = '/static/'
 USE_TZ = False
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
